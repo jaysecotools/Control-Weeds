22ce568bc9eed44b62d373c04acc04a4f396cb31
jaysecotools <jayrowley72@hotmail.com>
2026-05-04 21:12:49 +1000

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'weed-control-v2';
+const urlsToCache = [
+    './',
+    './index.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
